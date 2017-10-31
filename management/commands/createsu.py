7620061406e3fbcf06248f994fb7ffe4b427dc16
Shimbo Takaaki <ta7uwtaka@gmail.com>
2017-10-31 10:59:12 +0900

    Create management dir

---
+++ management/commands/createsu.py
@@ -0,0 +1,11 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth.models import User
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        if not User.objects.filter(username="admin").exists():
+            User.objects.create_superuser("user1", "user1@example.com", "user1")
+        else:
+            print("Already, admin user exists")
